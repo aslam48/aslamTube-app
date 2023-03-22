b7af40ec635983cb4da2edc63b7c8d829810d620
aslam48 <youngfizzy001@gmail.com>
2023-03-22 16:13:38 +0100

    working

--- src/components/MiniCard.js
+++ src/components/MiniCard.js
@@ -1,15 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView, TextInput, Dimensions} from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TextInput, Dimensions, Image} from 'react-native';
 
 
 const MiniCard = () => {
     return (
-        <View style={{ marginBottom: 8 ,elevation: 4, shadowOffset: {width: 10, height: 10,}, shadowColor: "black", shadowOpacity: 1.0 }}>
+        <View style={{ flexDirection: "row", margin: 10, marginBottom: 0}}>
         <Image source={{uri: "https://images.unsplash.com/photo-1679464307375-ad8ab6a70029?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80"}} 
-        style={{ width: "100%", height: 200}}
+        style={{ width: "45%", height: 100}}
         />
-        <View>
-            <Text style={{width:Dimensions.get("screen").width/2}} ellipsizeMode="tail" numberOfLines={3}> This is amazing course</Text>
+        <View style={{paddingLeft: 7}}>
+            <Text style={{width:Dimensions.get("screen").width/2,  fontSize: 17}} ellipsizeMode="tail" numberOfLines={3}> This is amazing course</Text>
             <Text style={{fontSize: 12}}>Coder never quite</Text>
         </View>
     </View>
--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,19 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView, TextInput} from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TextInput, Image, } from 'react-native';
 import {Ionicons} from '@expo/vector-icons';
 import { useState } from 'react';
+import MiniCart from '../components/MiniCard'
 
 const SearchScreen = () => {
     const [value, setValue] = useState(" ")
 return  (
     <View style={{flex: 1}}>
-        <View style={{ padding:5, flexDirection: "row", justifyContent:"space-aroun",elevation: 4, shadowOffset: {width: 10, height: 10,}, shadowColor: "black", shadowOpacity: 1.0}}>
+        <View style={{ padding:5, flexDirection: "row", justifyContent:"space-around",elevation: 4, shadowOffset: {width: 10, height: 10,}, shadowColor: "black", shadowOpacity: 1.0, backgroundColor:  "white"}}>
             <Ionicons name ="md-arrow-back" size={32} /> 
             <TextInput style={{width: "70%", backgroundColor: "#e6e6e6"}} value={value} onChangeText ={(text)=> setValue(text)} 
             />
             <Ionicons name ="md-send" size={32} /> 
-        
         </View>
+
+        <ScrollView>
+        <MiniCart/>
+        <MiniCart/>
+        <MiniCart/>
+        <MiniCart/>
+        <MiniCart/>
+        <MiniCart/>
+        <MiniCart/>
+        </ScrollView>
     </View>
 )
 }
