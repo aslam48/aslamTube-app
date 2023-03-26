c4b1dd1dadb4ddec645f905d7d234254ab2e8d8c
aslam48 <youngfizzy001@gmail.com>
2023-03-26 05:49:55 +0100

    working

--- src/components/MiniCard.js
+++ src/components/MiniCard.js
@@ -2,15 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, ScrollView, TextInput, Dimensions, Image} from 'react-native';
 
 
-const MiniCard = () => {
+const MiniCard = (props) => {
     return (
         <View style={{ flexDirection: "row", margin: 10, marginBottom: 0}}>
-        <Image source={{uri: "https://images.unsplash.com/photo-1679464307375-ad8ab6a70029?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80"}} 
+        <Image source={{uri: `https://i.ytimg.com/vi/${props.videoId}/hqdefault.jpg`}} 
         style={{ width: "45%", height: 100}}
         />
         <View style={{paddingLeft: 7}}>
-            <Text style={{width:Dimensions.get("screen").width/2,  fontSize: 17}} ellipsizeMode="tail" numberOfLines={3}> This is amazing course</Text>
-            <Text style={{fontSize: 12}}>Coder never quite</Text>
+            <Text style={{width:Dimensions.get("screen").width/2,  fontSize: 17}} ellipsizeMode="tail" numberOfLines={3}>{props.title}</Text>
+            <Text style={{fontSize: 12}}>{props.channel}</Text>
         </View>
     </View>
     )
--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,29 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView, TextInput, Image, } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TextInput, Image, FlatList, ActivityIndicator} from 'react-native';
 import {Ionicons} from '@expo/vector-icons';
 import { useState } from 'react';
 import MiniCart from '../components/MiniCard'
 
+
+
 const SearchScreen = () => {
     const [value, setValue] = useState(" ")
+    const [miniCarData, setminiCard] = useState([])
+    const [loading, setLoading] = useState(false)
+
+const fetchData = () => {
+    setLoading(true)
+    fetch(`
+https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${value}&type=video&key=AIzasyahhQMjGR-molv_AcoleMJKZYg8YMoFIVI`)
+    .then(res => res.json())
+    .then (data=> {
+        console.log(data)
+        setLoading(false)
+        setminiCard(data.items)
+    })
+}
 return  (
+   
     <View style={{flex: 1}}>
         <View style={{ padding:5, flexDirection: "row", justifyContent:"space-around",elevation: 4, shadowOffset: {width: 10, height: 10,}, shadowColor: "black", shadowOpacity: 1.0, backgroundColor:  "white"}}>
             <Ionicons name ="md-arrow-back" size={32} /> 
             <TextInput style={{width: "70%", backgroundColor: "#e6e6e6"}} value={value} onChangeText ={(text)=> setValue(text)} 
             />
-            <Ionicons name ="md-send" size={32} /> 
+            <Ionicons onPress={() => fetchData()} name ="md-send" size={32} /> 
         </View>
 
-        <ScrollView>
-        <MiniCart/>
-        <MiniCart/>
-        <MiniCart/>
-        <MiniCart/>
-        <MiniCart/>
-        <MiniCart/>
-        <MiniCart/>
-        </ScrollView>
+        { loading ?   <ActivityIndicator style={{marginTop: 10}} size='large' color='red'/> : null }
+
+        <FlatList data={miniCarData} renderItem={({item}) => { 
+        return <MiniCart
+         videoID={item.id.videoID} 
+        title={item.snippet.title}
+        channel={item.snippet.channelTitle}
+         />
+    }} 
+    keyExtractor={item => item.id.videoID}
+    />
+
     </View>
 )
 }
